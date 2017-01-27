bd10e88666411ab97d9c2b4a00d8fa308bc9ba97
Varun Ramachandran <varun.rama@gmail.com>
2017-01-27 17:01:08 -0500

    fix ur mistakes im basically jesus

--- app/src/main/java/com/hackgteeny/tastebuds/FriendActivity.java
+++ app/src/main/java/com/hackgteeny/tastebuds/FriendActivity.java
@@ -14,13 +14,13 @@ public class FriendActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
-        ll = (LinearLayout) findViewById(R.id.fl);
-        String[] fl = {"lol", "This", "is", "snek"};
-        for (String s : fl) {
-            TextView t = new TextView(this);
-            t.setText(s);
-            ll.addView(t);
-        }
+//        ll = (LinearLayout) findViewById(R.id.fl);
+//        String[] fl = {"lol", "This", "is", "snek"};
+//        for (String s : fl) {
+//            TextView t = new TextView(this);
+//            t.setText(s);
+//            ll.addView(t);
+//        }
 
 
     }
